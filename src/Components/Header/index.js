87029fed5caf1cdcb21c4ff53ff50b012858156a
Ermin Cahtarevic <erminc@windowslive.com>
2020-11-24 18:20:48 +0100

    Update mobile version of the app

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './header.css';
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1000);
+
+  const updateSize = () => {
+    setIsMobile(window.innerWidth < 1000);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  });
+
   return (
     <div className="header">
-      <img src='./images/bg-header-desktop.svg' alt="header" />
+      <img
+        src={isMobile ? './images/bg-header-mobile.svg' : './images/bg-header-desktop.svg'}
+        alt="header"
+      />
     </div>
   );
 };
--- src/Components/Item/index.js
+++ src/Components/Item/index.js
@@ -6,38 +6,40 @@ import './item.css';
 const Item = ({ job }) => {
   return (
     <div className="item">
-      <div className="item-left">
-        <div className={job.featured ? 'featured-line featured' : 'featured-line'} />
-        <img src={job.logo} className="job-logo" alt={job.company} />
-        <div className="job-description">
-          <div className="job-description-top">
-            <span>{job.company}</span>
-            { job.new && <span className="new-job">New!</span> }
-            { job.featured && <span className="featured-job">Featured</span> }
-          </div>
-          <h3>{job.position}</h3>
-          <div className="job-description-bottom">
-            <span>{job.postedAt}</span>
-            <span className="job-description-dot" />
-            <span>{job.contract}</span>
-            <span className="job-description-dot" />
-            <span>{job.location}</span>
+      <div className={job.featured ? 'featured-line featured' : 'featured-line'} />
+      <div className="content-wrap">
+        <div className="item-left">
+          <img src={job.logo} className="job-logo" alt={job.company} />
+          <div className="job-description">
+            <div className="job-description-top">
+              <span>{job.company}</span>
+              { job.new && <span className="new-job">New!</span> }
+              { job.featured && <span className="featured-job">Featured</span> }
+            </div>
+            <h3>{job.position}</h3>
+            <div className="job-description-bottom">
+              <span>{job.postedAt}</span>
+              <span className="job-description-dot" />
+              <span>{job.contract}</span>
+              <span className="job-description-dot" />
+              <span>{job.location}</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="item-right">
-        <Tag value={job.role} />
-        <Tag value={job.level} />
-        {
-          job.languages.map(language => (
-            <Tag key={language} value={language} />
-          ))
-        }
-        {
-          job.tools.map(tool => (
-            <Tag key={tool} value={tool} />
-          ))
-        }
+        <div className="item-right">
+          <Tag value={job.role} />
+          <Tag value={job.level} />
+          {
+            job.languages.map(language => (
+              <Tag key={language} value={language} />
+            ))
+          }
+          {
+            job.tools.map(tool => (
+              <Tag key={tool} value={tool} />
+            ))
+          }
+        </div>
       </div>
     </div>
   );
